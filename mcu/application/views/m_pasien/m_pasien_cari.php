93e60f8713cd63bf770dd9f822a48b8e6eeff919
angkelmuthu <angkel.muthugmail.com>
2020-03-17 22:06:32 +0700

    dell

--- mcu/application/views/m_pasien/m_pasien_cari.php
+++ mcu/application/views/m_pasien/m_pasien_cari.php
@@ -5,7 +5,7 @@
                 <div class="container py-4 py-lg-5 my-lg-5 px-4 px-sm-0">
                     <div class="row">
                         <div class="col-xl-12 mb-5">
-                            <h2 class="fs-xxl fw-500 mt-1 text-center">
+                            <h2 class="fs-xxl fw-500 mt-0 text-center">
                                 Pencarian Pasien
                                 <small class="h3 fw-300 mt-3 mb-5 opacity-70 hidden-sm-down">
                                     Untuk memudahkan pencarian data pasien terdapat fitur <strong>TAP CARD</strong>. <br>
@@ -13,75 +13,59 @@
                                 </small>
                             </h2>
                         </div>
-                        <div class="col-xl-12 mb-5">
-                            <ul class="nav nav-tabs nav-fill" role="tablist">
-                                <li class="nav-item"><a class="nav-link active" data-toggle="tab" href="#tab_justified-1" role="tab">Home</a></li>
-                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#tab_justified-2" role="tab">Profile</a></li>
-                                <li class="nav-item"><a class="nav-link" data-toggle="tab" href="#tab_justified-3" role="tab">Time</a></li>
-                                <li class="nav-item">
-                                    <a class="nav-link disabled" href="#" tabindex="-1">Disabled</a>
-                                </li>
-                            </ul>
-                            <div class="tab-content p-3">
-                                <div class="tab-pane fade show active" id="tab_justified-1" role="tabpanel">
-                                    Raw denim you probably haven't heard of them jean shorts Austin. Nesciunt tofu stumptown aliqua, retro synth master cleanse. Mustache cliche tempor, williamsburg carles vegan helvetica. Reprehenderit butcher retro keffiyeh dreamcatcher synth. Cosby sweater eu banh mi, qui irure terry richardson ex squid. Aliquip placeat salvia cillum iphone.
+                        <div class="col-xl-3 ml-auto mr-auto">
+                            <a class="card bg-primary text-white text-center p-3" onclick="toggleVisibility('tab');">
+                                <h2>Tap Kartu Pasien atau E-KTP</h2>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 ml-auto mr-auto">
+                            <a class="card bg-info text-white text-center p-3" onclick="toggleVisibility('by');">
+                                <h2>Pencarian Data<br>Pasien</h2>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 ml-auto mr-auto">
+                            <a href="<?php echo base_url(); ?>m_pasien" class="card bg-success text-white text-center p-3">
+                                <h2>Tampilkan Seluruh Data Pasien</h2>
+                            </a>
+                        </div>
+                        <div class="col-xl-3 ml-auto mr-auto">
+                            <a href="<?php echo base_url(); ?>m_pasien/create/Y" class="card bg-warning text-white text-center p-3">
+                                <h2>Pendaftaran Pasien Baru</h2>
+                            </a>
+                        </div>
+                        <div id="tab" class="col-xl-12">
+                            <div class="pt-4">
+                                <div class="alert bg-primary-500" role="alert">
+                                    <h5>Tap Kartu Pasien atau E-KTP</h5>
                                 </div>
-                                <div class="tab-pane fade" id="tab_justified-2" role="tabpanel">
-                                    Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid. Exercitation +1 labore velit, blog sartorial PBR leggings next level wes anderson artisan four loko farm-to-table craft beer twee. Qui photo booth letterpress, commodo enim craft beer mlkshk aliquip jean shorts ullamco ad vinyl cillum PBR. Homo nostrud organic.
-                                </div>
-                                <div class="tab-pane fade" id="tab_justified-3" role="tabpanel">
-                                    Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed craft beer, iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Leggings gentrify squid 8-bit cred pitchfork.
+                                <div class="input-group input-group-lg mb-5 shadow-1 rounded">
+                                    <input type="text" id="tap" class="form-control shadow-inset-2" id="filter-icon" aria-label="type 2 or more letters" placeholder="tap kartu pasien atau ektp pasien" autofocus>
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary hidden-sm-down" type="button"><i class="fal fa-id-card-alt mr-lg-2"></i><span class="hidden-md-down">TAP</span></button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <!-- <div class="col-xl-4 ml-auto mr-auto"> -->
-                            <!-- <div class="card bg-primary text-white text-center p-3">
-                                <h2>Tap Kartu Pasien atau E-KTP</h2>
-                            </div> -->
-                            <!-- <div class="card p-2 bg-faded">
-                                <div class="d-flex flex-column align-items-center justify-content-center p-2">
-                                    <img src="<?php echo base_url() ?>assets/smartadmin/img/demo/php.png" class="rounded shadow-2 img-thumbnail" alt="">
-                                    <h5 class="mb-0 fw-700 text-center mt-3">
-                                        Card Less
-                                        <small class="text-muted mb-0">Tap Kartu Pasien atau E-KTP</small>
-                                    </h5>
+                        <div id="by" class="col-xl-12 ml-auto mr-auto" style="display: none;">
+                            <div class="pt-4">
+                                <div class="alert text-white bg-info-500" role="alert">
+                                <h5>Pencarian Data Pasien</h5>
                                 </div>
-                                <button class="btn btn-block btn-primary">Pilih</button>
-                            </div> -->
-                        <!-- </div> -->
+                                <!-- <h1 class="mb-2">
+                                    <small class="mb-1">
+                                        Request time (0.23 seconds)
+                                    </small>
+                                </h1> -->
+                                <div class="input-group input-group-lg mb-5 shadow-1 rounded">
+                                    <input type="text" class="form-control shadow-inset-2" id="filter-icon" aria-label="type 2 or more letters" placeholder="Search anything...">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary hidden-sm-down" type="button"><i class="fal fa-search mr-lg-2"></i><span class="hidden-md-down">Search</span></button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
-                        <!-- <div class="col-xl-4 ml-auto mr-auto">
-                            <a href="m_pasien/list" class="card bg-success text-white text-center p-3">
-                                <h2>Tampilkan Seluruh Data Pasien</h2>
-                            </a> -->
-                            <!-- <div class="card p-2 bg-faded">
-                                <div class="d-flex flex-column align-items-center justify-content-center p-2">
-                                    <img src="<?php echo base_url() ?>assets/smartadmin/img/demo/php.png" class="rounded shadow-2 img-thumbnail" alt="">
-                                    <h5 class="mb-0 fw-700 text-center mt-3">
-                                        All Data
-                                        <small class="text-muted mb-0">Tampilkan seluruh data pasien</small>
-                                    </h5>
-                                </div>
-                                <a href="m_pasien/list" class="btn btn-block btn-primary">Pilih</a>
-                            </div> -->
-                        <!-- </div> -->
-
-                        <!-- <div class="col-xl-4 ml-auto mr-auto">
-                            <a href="m_pasien/create/Y" class="card bg-info text-white text-center p-3">
-                                <h2>Pendaftaran Pasien Baru</h2>
-                            </a> -->
-                            <!-- <div class="card p-2 bg-faded">
-                                <div class="d-flex flex-column align-items-center justify-content-center p-2">
-                                    <img src="<?php echo base_url() ?>assets/smartadmin/img/demo/php.png" class="rounded shadow-2 img-thumbnail" alt="">
-                                    <h5 class="mb-0 fw-700 text-center mt-3">
-                                        Pendaftaran Pasien Baru
-                                        <small class="text-muted mb-0">Untuk pendaftaran baru pilih metode ini</small>
-                                    </h5>
-                                </div>
-                                <a href="m_pasien/create/Y" class="btn btn-block btn-primary">Pilih</a>
-                            </div> -->
-                        <!-- </div> -->
-                    <!-- </div> -->
+                    </div>
                 </div>
             </div>
         </div>
@@ -89,3 +73,28 @@
 </main>
 <script src="<?php echo base_url() ?>assets/smartadmin/js/vendors.bundle.js"></script>
 <script src="<?php echo base_url() ?>assets/smartadmin/js/app.bundle.js"></script>
+<script>
+var divs = ["tab", "by"];
+var visibleDivId = null;
+function toggleVisibility(divId) {
+  if(visibleDivId === divId) {
+    //visibleDivId = null;
+  } else {
+    visibleDivId = divId;
+  }
+  hideNonVisibleDivs();
+}
+function hideNonVisibleDivs() {
+  var i, divId, div;
+  for(i = 0; i < divs.length; i++) {
+    divId = divs[i];
+    div = document.getElementById(divId);
+    if(visibleDivId === divId) {
+      div.style.display = "block";
+      document.getElementById("tap").focus();
+    } else {
+      div.style.display = "none";
+    }
+  }
+}
+</script>
